0cea9cf9e169e81c85e153a6677a6f27ba6d6612
v.lelicanin <v.lelicanin@sae.edu>
2020-03-27 19:30:18 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+// var user;
+
+// if(ime == 'vlada') {
+//     user = 'vlada';
+// } else {
+//     user = 'guest';
+// }
+// user = (ime == 'vlada') ? 'vlada' : 'guest';
+
 $('li:last-child').prependTo('ul');
 
+var interval = setInterval(function(){
+    moveSlides('forward');
+}, 2000);
+
+$('button').on('click', function () {
+    clearInterval(interval);
+})
 
 $('.next').on('click', function(){
     moveSlides('forward');
@@ -9,16 +25,25 @@ $('.prev').on('click', function(){
     moveSlides('backward')
 });
 
+
+/**
+ * Move slides in any direction
+ *
+ * @param direction (can be "forward" or "backward")
+ */
 function moveSlides(direction) {
-    $('.prev').prop('disabled', true);
-    $('li:last-child').prependTo('ul');
-    $('ul').css('left', '-=300px');
+    var button = (direction == "forward") ? '.next' : '.prev';
+    $(button).prop('disabled', true);
+    (direction == "forward") ?  $('li:first-child').appendTo('ul') : $('li:last-child').prependTo('ul');
+    var signCancel = (direction == 'forward') ? "+" : '-';
+    var signAnimate = (direction == 'forward') ? "-" : '+';
+    $('ul').css('left', signCancel + '=300px');
 
     $('ul').animate({
-        left: '+=300px'
+        left: signAnimate + '=300px'
     }, {
         done: function(){
-            $('.prev').prop('disabled', false);
+            $(button).prop('disabled', false);
         }
     });
 }
